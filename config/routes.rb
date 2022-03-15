481ec327b40bc2f62e2d879c469fc837c3d1956a
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-15 12:22:44 +0100

    added style to unsigned homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :categories
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'categories#index'
 end
