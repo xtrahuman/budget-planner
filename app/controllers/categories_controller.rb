748a2d71cc01a97dca44f4cd44c690c5b94827c2
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-16 04:08:02 +0100

    styling of navigation and add category button

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,25 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @author = current_user
+    @categories = Category.all
+  end
+
+  def new
+      @category = Category.new
+  end
+
+  def create
+    @category = Category.new
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to category_path, notice: 'Category was created successfully.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def category_params
+    params.require(:food).permit(:name, :icon)
+  end
 end
