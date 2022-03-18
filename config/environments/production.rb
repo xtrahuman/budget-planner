bdb4b73e1001f8c14e6c14681691f053bd0c1e50
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-18 16:43:49 +0100

    fix mailer host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'https://immense-bastion-80345.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'immense-bastion-80345.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
