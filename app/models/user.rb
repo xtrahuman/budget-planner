5a781a02adcda11593f879706f932a928cf3aae6
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-18 17:10:35 +0100

    fixed linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   def admin?(requested_role)
     role == requested_role.to_s
